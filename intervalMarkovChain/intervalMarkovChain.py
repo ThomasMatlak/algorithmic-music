47e79f190f2f4bf8177f5733839878417a2054cd
Thomas Matlak <thomas.matlak@gmail.com>
2017-09-27 21:14:37 -0400

    added basic rhythm generation

--- intervalMarkovChain/intervalMarkovChain.py
+++ intervalMarkovChain/intervalMarkovChain.py
@@ -39,13 +39,14 @@ def main():
 
     my_normalized_score = normalize_score(my_score)
 
-    # create a Markov Chain
-    # transition matrix
-    transition_counts = defaultdict(lambda :defaultdict(int))
+    # create Markov Chains for intervals and rhythms
+    interval_transitions = defaultdict(lambda :defaultdict(int))
+    rhythm_transitions = defaultdict(lambda :defaultdict(int))
 
     prev_note = None
     interval = None
 
+    # for note in my_normalized_score.getElementsByClass(m21.note.Note):
     for note in my_normalized_score[0].getElementsByClass(m21.note.Note):
         if prev_note is None:
             prev_note = note
@@ -56,8 +57,9 @@ def main():
             continue
         else:
             current_interval = m21.interval.Interval(prev_note, note)
-            transition_counts[interval.directedName][current_interval.directedName] += 1
+            interval_transitions[interval.directedName][current_interval.directedName] += 1
             interval = current_interval
+            rhythm_transitions[prev_note.quarterLength][note.quarterLength] += 1
             prev_note = note
 
     # Set up the score
@@ -68,38 +70,61 @@ def main():
 
     # seed the melody with the first two notes; melody will start on tonic in the 5th octave
     generated_notes = [m21.note.Note(m21.pitch.Pitch(0, octave=5), quarterLength=1)]
-    interval = m21.interval.Interval(list(transition_counts.items())[random.randint(0, len(transition_counts))][0])
+    interval = m21.interval.Interval(list(interval_transitions.items())[random.randint(0, len(interval_transitions) - 1)][0])
     generated_notes.append(generated_notes[0].transpose(interval))
+    generated_notes[1].quarterLength = 1
 
     count = 2
+    beats = 2.0
 
     # we want the melody to be a multiple of 4 beats that is >= 8 beats, ending on a pitch in the i chord
-    # while count < 16 or count % 4 != 0 or not (generated_notes[count - 1].name == 'A' or # minor
+    # while beats < 16 or beats % 4 != 0 or not (generated_notes[count - 1].name == 'A' or # minor
     #                                            generated_notes[count - 1].name == 'C' or
     #                                            generated_notes[count - 1].name == 'E'):
-    while count < 16 or count % 4 != 0 or not (generated_notes[count - 1].name == 'C' or # Major
+    while beats < 32 or beats % 4 != 0 or not (generated_notes[count - 1].name == 'C' or # Major
                                                generated_notes[count - 1].name == 'E' or
                                                generated_notes[count - 1].name == 'G'):
-        transition_sum = 0
-        for p in transition_counts[interval.directedName]:
-            transition_sum += transition_counts[interval.directedName][p]
+        transition_sum = 0.0
+        for p in interval_transitions[interval.directedName]:
+            transition_sum += interval_transitions[interval.directedName][p]
 
-        current_probabilities = {}
-        for key in list(transition_counts[interval.directedName].keys()):
-            current_probabilities[key] = float(transition_counts[interval.directedName][key] / float(transition_sum))
+        rhythm_sum = 0.0
+        for p in rhythm_transitions[generated_notes[count - 1].quarterLength]:
+            rhythm_sum += rhythm_transitions[generated_notes[count - 1].quarterLength][p]
 
-        rand_num = random.random()
+        curr_interval_probabilities = {}
+        for key in list(interval_transitions[interval.directedName].keys()):
+            curr_interval_probabilities[key] = interval_transitions[interval.directedName][key] / transition_sum
 
-        probability_sum = 0.0
+        curr_rhythm_probabilities = {}
+        for key in list(rhythm_transitions[generated_notes[count - 1].quarterLength].keys()):
+            curr_rhythm_probabilities[key] = rhythm_transitions[generated_notes[count - 1].quarterLength][key] / rhythm_sum
 
-        for index in current_probabilities:
-            probability_sum += current_probabilities[index]
+        interval_rand_num = random.random()
+        rhythm_rand_num = random.random()
 
-            if rand_num <= probability_sum:
+        interval_probability_sum = 0.0
+        rhythm_probability_sum = 0.0
+
+        my_note = None
+
+        for index in curr_interval_probabilities:
+            interval_probability_sum += curr_interval_probabilities[index]
+
+            if interval_rand_num <= interval_probability_sum:
                 interval = m21.interval.Interval(index)
-                generated_notes.append(generated_notes[count - 1].transpose(interval))
+                my_note = generated_notes[count - 1].transpose(interval)
                 break
 
+        for index in curr_rhythm_probabilities:
+            rhythm_probability_sum += curr_rhythm_probabilities[index]
+
+            if rhythm_rand_num <= rhythm_probability_sum:
+                my_note.quarterLength = index
+
+        generated_notes.append(my_note)
+
+        beats += my_note.quarterLength
         count += 1
 
     part = m21.stream.Part()
